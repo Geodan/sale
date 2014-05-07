7faaa9f0cb437afabcfc408931d26c65111ebf10
sailingnetworks <mark.opmeer@geodan.nl>
2014-05-07 16:38:07 +0200

    json

--- jsoncreator.php
+++ jsoncreator.php
@@ -18,62 +18,22 @@ if (!$db_selected)
   die ('Can\'t use db : ' . mysql_error());
 } 
 
- // Selects all the rows in the markers table.
- $query = 'SELECT * FROM networks WHERE source = "Jan van Ruijven"';
- $result = mysql_query($query);
- if (!$result)
- {
-  die('Invalid query: ' . mysql_error());
- }
+$return_arr = array();
 
-// Creates an array of strings to hold the lines of the KML file.
-$kml = array('<?xml version="1.0" encoding="UTF-8"?>');
-$kml[] = ' <kml>';
-/*$kml[] = '<kml xmlns="http://earth.google.com/kml/2.1">';
-$kml[] = ' <Document>';
-$kml[] = ' <Style id="restaurantStyle">';
-$kml[] = ' <IconStyle id="restuarantIcon">';
-$kml[] = ' <Icon>';
-$kml[] = ' <href>http://maps.google.com/mapfiles/kml/pal2/icon63.png</href>';
-$kml[] = ' </Icon>';
-$kml[] = ' </IconStyle>';
-$kml[] = ' </Style>';
-$kml[] = ' <Style id="barStyle">';
-$kml[] = ' <IconStyle id="barIcon">';
-$kml[] = ' <Icon>';
-$kml[] = ' <href>http://maps.google.com/mapfiles/kml/pal2/icon27.png</href>';
-$kml[] = ' </Icon>';
-$kml[] = ' </IconStyle>';
-$kml[] = ' </Style>'; */
+$fetch = mysql_query('SELECT * FROM networks WHERE source = "Jan van Ruijven"'); 
 
-// Iterates through the rows, printing a node for each row.
-while ($row = @mysql_fetch_assoc($result))
-{
-  $kml[] = ' <Placemark>';
-  $kml[] = ' <id>' . ($row['idmeertens']) .'</id>';
-  $kml[] = ' <name> Geadresseerde: ' . htmlentities($row['target']). '</name>';
-  $kml[] = ' <description> Schip: ' . ($row['schip']) . '</description>';
-  $kml[] = ' <locations>';
-  $kml[] = ' <lon>' . $row['lng'] . '</lon>';
-  $kml[] = ' <lat>' . $row['lat'] . '</lat>';
-  $kml[] = ' <place>' . $row['bestemming_locatie'] . '</place>';
-  $kml[] = ' </locations>';
-  $kml[] = ' <senderlocations>';
-  $kml[] = ' <lon>' . $row['lng_afzender'] . '</lon>';
-  $kml[] = ' <lat>' . $row['lat_afzender'] . '</lat>';
-  $kml[] = ' <place>' . $row['verzendlocatie'] . '</place>';
-  $kml[] = ' </senderlocations>';
-  $kml[] = ' <time>' .$row['datum_verzonden'] .'</time>';
-  $kml[] = ' <tablecontent>';
-  $kml[] = ' <beroepafzender>' .$row['beroep_afzender'] .'</beroepafzender>';
-  $kml[] = ' </tablecontent>';
-  $kml[] = ' </Placemark>';
+while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
+    $row_array['id'] = $row['idmeertens'];
+    $row_array['name'] = $row['target'];
+    $row_array['description'] = $row['genoemde_namen'];
+	$row_array['lon'] = $row['lng'];
+	$row_array['lat'] = $row['lat'];
+	$row_array['place'] = $row['bestemming_locatie'];
+	$row_array['time'] = $row['datum_verzonden'];
+	$row_array['tableContent'] = $row['genre'];
+
+    array_push($return_arr,$row_array);
 }
 
-// End XML file
-//$kml[] = ' </Document>';
-$kml[] = '</kml>';
-$kmlOutput = join("\n", $kml);
-header('Content-type: application/vnd.google-earth.kml+xml');
-echo $kmlOutput;
+echo json_encode($return_arr);
 ?>
