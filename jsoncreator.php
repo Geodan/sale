2f54bd7bcf4e65fed2b89190e322e060e68d6b56
sailingnetworks <mark.opmeer@geodan.nl>
2014-10-20 13:55:55 +0200

    jsoncreator and level of detail

--- jsoncreator.php
+++ jsoncreator.php
@@ -20,17 +20,28 @@ if (!$db_selected)
 
 $return_arr = array();
 
-$fetch = mysql_query('SELECT * FROM networks WHERE source = "Jan van Ruijven"'); 
+$fetch = mysql_query('SELECT * FROM networks WHERE landcode = 1 AND fuzzy = 1'); 
 
 while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
     $row_array['id'] = $row['idmeertens'];
+	$row_array['source'] = $row['source'];
+	$row_array['target'] = $row['target'];
     $row_array['name'] = $row['target'];
     $row_array['description'] = $row['genoemde_namen'];
-	$row_array['lon'] = $row['lng'];
-	$row_array['lat'] = $row['lat'];
-	$row_array['place'] = $row['bestemming_locatie'];
+	$row_array['location'] = [];
+	$object1 = new StdClass();
+	$object1->lon = $row['lng_ontvanger'];
+	$object1 ->lat = $row['lat_ontvanger'];
+	$object1 ->place = $row['bestemming_locatie'];
+	$object2 = new StdClass();
+	$object2 ->lon = $row['lng_zender']; 
+	$object2 ->lat = $row['lat_zender']; 
+	$object2 ->place = $row['verzendlocatie'];
+	array_push($row_array['location'], $object1);
+	array_push($row_array['location'], $object2);
 	$row_array['time'] = $row['datum_verzonden'];
-	$row_array['tableContent'] = $row['genre'];
+	$row_array['fuzzy'] = $row['fuzzy'];
+	$row_array['tableContent'] = [$row['source'], $row['target'], $row['datum_verzonden'], $row['bestemming_locatie']] ;
 
     array_push($return_arr,$row_array);
 }
